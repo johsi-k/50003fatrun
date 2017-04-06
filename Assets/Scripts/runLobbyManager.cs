e61a9e7f6a739e85ca2d832371a1384705430e58
johsi-k <johsi.k@gmail.com>
2017-04-07 01:25:23 +0800

    runLobbyManager changes
    first attempt to support matchmaking

--- Assets/Scripts/runLobbyManager.cs
+++ Assets/Scripts/runLobbyManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
@@ -13,11 +14,102 @@ public class runLobbyManager : NetworkLobbyManager {
     public Text mytext = null;
     int counter = 1;
 
+    public enum NetworkState
+    {
+        Inactive,
+        Pregame,
+        Connecting,
+        InLobby,
+        InGame
+    }
+
+    public NetworkState state
+    {
+        get;
+        protected set;
+    }
+
+    private Action<bool, MatchInfo> m_NextMatchCreatedCallback;
+    private Action<bool, MatchInfo> m_NextMatchJoinedCallback;
+
+    // called when we've created a match
+    public event Action<bool, MatchInfo> matchCreated; 
+
+    // called when a matchmade game is joined
+    public event Action<bool, MatchInfo> matchJoined;
+
+
     //public RawImage player1img,player2img,player3img,player4img;
     //public Texture playeron1;
 
     private void Update()
     {
+
+    }
+
+    public void onCreateClicked() // bound to button which creates a match for lobby to be visible
+    {
+        this.StartMatchMaker();
+        matchMaker.CreateMatch("gameName", 4, true, "matchPassword", "", "", 0, 0, OnMatchCreate); // dummy parameters
+       
+    }
+
+    public override void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        base.OnMatchCreate(success, extendedInfo, matchInfo);
+        Debug.Log("OnMatchCreate");
+
+        if (success) {
+            state = NetworkState.InLobby;
+        }
+
+        else
+        {
+            state = NetworkState.Inactive;
+        }
+
+        // Fire callback
+        if (m_NextMatchCreatedCallback != null)
+        {
+            m_NextMatchCreatedCallback(success, matchInfo);
+            m_NextMatchCreatedCallback = null;
+        }
+
+        // Fire event
+        if (matchCreated != null)
+        {
+            matchCreated(success, matchInfo);
+        }
+
+    }
+
+
+    public override void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        base.OnMatchJoined(success, extendedInfo, matchInfo);
+        Debug.Log("OnMatchJoined");
+
+        if (success)
+        {
+            state = NetworkState.InLobby;
+        }
+        else
+        {
+            state = NetworkState.Pregame;
+        }
+
+        // Fire callback
+        if (m_NextMatchJoinedCallback != null)
+        {
+            m_NextMatchJoinedCallback(success, matchInfo);
+            m_NextMatchJoinedCallback = null;
+        }
+
+        // Fire event
+        if (matchJoined != null)
+        {
+            matchJoined(success, matchInfo); 
+        }
 
     }
 
